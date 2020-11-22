ba7312713fce86167dd5dbac269b1bd945b423a1
swcg1 <stephaniewcheng@gmail.com>
2020-11-23 04:41:05 +0800

    complete spider chart with hover effects

--- js/GenreVis.js
+++ js/GenreVis.js
@@ -9,7 +9,7 @@ class GenreVis{
     initVis() {
         let vis = this;
 
-        vis.margin = { top: 50, right: 20, bottom: 20, left: 50 };
+        vis.margin = { top: 90, right: 20, bottom: 20, left: 50 };
         vis.width =
             $("#" + vis.parentElement).width() - vis.margin.left - vis.margin.right;
         vis.height =
@@ -37,6 +37,9 @@ class GenreVis{
         // define color scale
         vis.color = d3.scaleOrdinal()
             .range(["#66a1ed","#7de39f","#E50914","#edbc51"]);
+
+        let platforms = ["Disney+", "#19cf75", "Netflix", "Prime Video"];
+
 
         // vis.tooltip = d3
         //     .select("body")
@@ -112,23 +115,27 @@ class GenreVis{
             const total = vis.platforms[platform].size
             vis.genreBreakdown[platform].push({
                 "genre": genre,
-                "percent": vis.genreCount[movie]/total*100
+                "percent": vis.genreCount[movie]/total,
+                "platform": platform
             })
-            vis.maxCounter.push(vis.genreCount[movie]/total*100)
+            vis.maxCounter.push(vis.genreCount[movie]/total)
         })
 
         // Append genres that disney+ doesnt have
         vis.genreBreakdown["Disney+"].push({
             genre: "Film-Noir",
-            percent: 0
+            percent: 0,
+            platform: "Disney+"
         },
             {
                 genre: "Game-Show",
-                percent: 0
+                percent: 0,
+                platform: "Disney+"
             },
             {
                 genre: "Talk-Show",
-                percent: 0
+                percent: 0,
+                platform: "Disney+"
             })
 
         console.log(vis.genreBreakdown["Disney+"])
@@ -148,13 +155,33 @@ class GenreVis{
         console.log(vis.arrayGenre)
 
 
+        // vis.updateVis();
+        vis.filterData();
+        vis.filtering = "";
+    }
+
+    filterData(){
+        let vis = this;
+        vis.displayData = vis.arrayGenre;
+
+        let platforms = ["Disney+", "Hulu", "Netflix", "Prime Video"];
+
+        // if (vis.filtering){
+        //     if (platforms.indexOf(vis.filtering) != -1){
+        //         var indexOfFilter = platforms.indexOf(vis.filtering);
+        //         console.log(vis.filtering)
+        //     }
+        // }
 
         vis.updateVis();
+
     }
 
     updateVis(){
 
         let vis = this;
+
+        let platforms = ["Disney+", "Hulu", "Netflix", "Prime Video"];
 
         const max = Math.max;
         const sin = Math.sin;
@@ -168,13 +195,13 @@ class GenreVis{
             margin: vis.margin, //The margins of the SVG
             levels: 10,				//How many levels or inner circles should there be drawn
             maxValue: d3.max(vis.maxCounter), 			//What is the value that the biggest circle will represent
-            labelFactor: 1.25, 	//How much farther than the radius of the outer circle should the labels be placed
+            labelFactor: 1.15, 	//How much farther than the radius of the outer circle should the labels be placed
             wrapWidth: 60, 		//The number of pixels after which a label needs to be given a new line
             opacityArea: 0.35, 	//The opacity of the area of the blob
             dotRadius: 4, 			//The size of the colored circles of each blog
             opacityCircles: 0.1, 	//The opacity of the circles of each blob
             strokeWidth: 2, 		//The width of the stroke around each blob
-            roundStrokes: false,	//If true the area and stroke will follow a round path (cardinal-closed)
+            roundStrokes: true,	//If true the area and stroke will follow a round path (cardinal-closed)
             color: vis.color	//Color function
         };
 
@@ -182,7 +209,7 @@ class GenreVis{
         var allAxis = (vis.genreBreakdown.Netflix.map(function(i, j){return i.genre})),	//Names of each axis
             total = allAxis.length,					//The number of different axes
             radius = Math.min(cfg.w/2, cfg.h/2), 	//Radius of the outermost circle
-            Format = d3.format('%'),			 	//Percentage formatting
+            Format = d3.format('.0%'),			 	//Percentage formatting
             angleSlice = Math.PI * 2 / total;		//The width in radians of each "slice"
 
 
@@ -216,17 +243,6 @@ class GenreVis{
             .style("fill-opacity", cfg.opacityCircles)
             .style("filter" , "url(#glow)");
 
-        //Text indicating at what % each level is
-        // axisGrid.selectAll(".axisLabel")
-        //     .data(d3.range(1,(cfg.levels+1)).reverse())
-        //     .enter().append("text")
-        //     .attr("class", "axisLabel")
-        //     .attr("x", 4)
-        //     .attr("y", function(d){return -d*radius/cfg.levels;})
-        //     .attr("dy", "0.4em")
-        //     .style("font-size", "10px")
-        //     .attr("fill", "#737373")
-        //     .text(function(d,i) { return Format(cfg.maxValue * d/cfg.levels); });
 
         //Create the straight lines radiating outward from the center
         var axis = axisGrid.selectAll(".axis")
@@ -296,45 +312,175 @@ class GenreVis{
 
         //Create a wrapper for the blobs
         var blobWrapper = g.selectAll(".radarWrapper")
-            .data(vis.arrayGenre)
+            .data(vis.displayData)
             .enter().append("g")
             .attr("class", "radarWrapper");
 
-
         //Append the backgrounds
-        blobWrapper
+        var blobAreas = blobWrapper
             .append("path")
+            .merge(blobWrapper)
             .attr("class", "radarArea")
+            .attr("id", function(d,i){
+                return "blob" + i;
+            })
             .attr("d", function(d,i) { return radarLine(d); })
             .style("fill", (d,i) => cfg.color(i))
             .style("fill-opacity", cfg.opacityArea)
             .on('mouseover', function(d, i) {
                 //Dim all blobs
                 vis.svg.selectAll(".radarArea")
-                    .transition().duration(200)
-                    .style("fill-opacity", 0.1);
+                    // .transition().duration(200)
+                    .style("fill-opacity", 0.05);
+
+                vis.svg.selectAll(".radarStroke")
+                    .style("stroke-opacity",0.1)
+
+                // console.log(platforms.indexOf(i[0].platform))
+
+                const idx = platforms.indexOf(i[0].platform)
+
+                vis.svg.select("#stroke" + idx)
+                    .style("stroke-opacity", 1)
+
+                vis.svg.select("#label"+idx)
+                    .attr("fill",vis.color(idx))
+                    .attr("fill-opacity", 0.5)
+
+                // vis.svg.select("#label"+i)
+                //     .style("fill", cfg.color(i))
+
                 //Bring back the hovered over blob
                 d3.select(this)
-                    .transition().duration(200)
-                    .style("fill-opacity", 0.7);
+                    // .transition().duration(200)
+                    .style("fill-opacity", 1);
+
+
             })
-            .on('mouseout', () => {
+            .on('mouseout', (d,i) => {
                 //Bring back all blobs
                 vis.svg.selectAll(".radarArea")
-                    .transition().duration(200)
+                    // .transition().duration(200)
                     .style("fill-opacity", cfg.opacityArea);
+
+                vis.svg.selectAll(".radarStroke")
+                    .style("stroke-opacity",1)
+
+                const idx = platforms.indexOf(i[0].platform)
+
+                vis.svg.select("#label"+idx)
+                    .attr("fill",vis.color(idx))
+                    .attr("fill-opacity", 0.1)
             });
 
         //Create the outlines
         blobWrapper.append("path")
             .attr("class", "radarStroke")
+            .attr("id", function(d,i){
+                return "stroke" + i;
+            })
             .attr("d", function(d,i) { return radarLine(d); })
             .style("stroke-width", cfg.strokeWidth + "px")
             .style("stroke", (d,i) => cfg.color(i))
             .style("fill", "none")
             .style("filter" , "url(#glow)");
 
-        // //Append the circles
+        // add hover buttons
+        vis.labels = vis.svg.append("g")
+            .attr("class", "platform-labels")
+
+        vis.labelButtons = vis.labels.selectAll(".platform-labels-rect")
+            .data(platforms)
+            .enter();
+
+        vis.labels.selectAll(".platforms-labels-text")
+            .data(platforms)
+            .enter()
+            .append("text")
+            .attr("class", "platform-labels-text")
+            .attr("x", function(d,i){
+                return (vis.width)/4*i+60
+            })
+            .attr("y", -25)
+            .text(function(d,i){return d})
+            .attr("fill", "black")
+            .attr("dominant-baseline","middle")
+            .attr("text-anchor","middle");
+
+        vis.labelButtons
+            .append("rect")
+            .attr("class", "platform-labels-rect")
+            .attr("width", 120)
+            .attr("height", 50)
+            .attr("stroke-width", 2)
+            .attr("stroke", function(d,i){
+                return vis.color(i)
+            })
+            .attr("fill",function(d,i){
+                return vis.color(i)
+            })
+            .attr("fill-opacity", 0.1)
+            .attr("x", function(d,i){
+                return (vis.width)/4*i
+            })
+            .attr("y", -50)
+            .attr("rx", 5)
+            .attr("id", function(d,i){
+                return "label" + i
+            })
+            .on("mouseover", function(d,i){
+
+                //Dim all blobs
+                vis.svg.selectAll(".radarArea")
+                    // .transition().duration(200)
+                    .style("fill-opacity", 0.05);
+
+                vis.svg.selectAll(".radarStroke")
+                    .style("stroke-opacity",0.2)
+
+                d3.select(this).attr("fill-opacity",0.5)
+
+                const idx = platforms.indexOf(i)
+                // // highlight blob
+
+                vis.svg.select("#stroke" + idx)
+                    .style("stroke-opacity", 1)
+
+                vis.svg.selectAll("#blob" + idx)
+                    .style("fill-opacity", 1);
+            })
+            .on("mouseout", function(d,i){
+
+                //Dim all blobs
+                vis.svg.selectAll(".radarArea")
+                    // .transition().duration(200)
+                    .style("fill-opacity", cfg.opacityArea);
+
+                vis.svg.selectAll(".radarStroke")
+                    .style("stroke-opacity",1)
+
+                d3.select(this).attr("fill-opacity",0.1)
+            })
+            .on('click', function(data,i){
+                // vis.filtering = (vis.filtering) ? "" : i;
+                // vis.filterData();
+                // blobWrapper.exit().remove();
+        });
+
+        // Text indicating at what % each level is
+        g.append("g").selectAll(".axisLabel")
+            .data(d3.range(1,(cfg.levels+1)).reverse())
+            .enter().append("text")
+            .attr("class", "axisLabel")
+            .attr("x", 4)
+            .attr("y", function(d){return -d*radius/cfg.levels;})
+            .attr("dy", "0.4em")
+            .style("font-size", "10px")
+            .attr("fill", "#737373")
+            .text(function(d,i) { return Format(cfg.maxValue * d/cfg.levels); });
+
+
+        // // Append the circles
         // blobWrapper.selectAll(".radarCircle")
         //     .data(vis.arrayGenre[0])
         //     .enter()
@@ -344,7 +490,7 @@ class GenreVis{
         //     .attr("cx", (d,i) => rScale(d.percent) * Math.cos(angleSlice * i - Math.PI / 2))
         //     .attr("cy", (d,i) => rScale(d.percent) * Math.sin(angleSlice * i - Math.PI / 2))
         //     .style("fill", (d) => cfg.color(d.id))
-        //     .style("fill-opacity", 0.8);
+        //     .style("fill-opacity", 0);
 
     }
 }

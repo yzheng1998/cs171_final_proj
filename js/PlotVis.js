42e0c1a87e3a9eabbfec1d154d635ed25c9165cb
Yuke Zheng <yukezheng@Yukes-MacBook-Pro-2.local>
2020-12-06 23:54:26 -0500

    done

--- js/PlotVis.js
+++ js/PlotVis.js
@@ -34,8 +34,7 @@ class PlotVis {
 
     vis.y = d3.scaleLinear().range([vis.height, 0]);
 
-    vis.xAxis = d3.axisBottom().scale(vis.x)
-        .tickFormat(d3.format(".0%"));
+    vis.xAxis = d3.axisBottom().scale(vis.x).tickFormat(d3.format(".0%"));
 
     vis.yAxis = d3.axisLeft().scale(vis.y);
 
@@ -46,22 +45,29 @@ class PlotVis {
 
     vis.svg.append("g").attr("class", "y-axis axis");
 
-    vis.svg.append("text")
-        .attr("transform",
-            "translate(" + (vis.width/2) + " ," +
-            (vis.height + vis.margin.top + 10) + ")")
-        .style("text-anchor", "middle")
-        .style('font-size', '12px')
-        .text("Rotten Tomatoes Score");
+    vis.svg
+      .append("text")
+      .attr(
+        "transform",
+        "translate(" +
+          vis.width / 2 +
+          " ," +
+          (vis.height + vis.margin.top + 10) +
+          ")"
+      )
+      .style("text-anchor", "middle")
+      .style("font-size", "12px")
+      .text("Rotten Tomatoes Score");
 
-    vis.svg.append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 0 - vis.margin.left)
-        .attr("x",0 - (vis.height / 2))
-        .attr("dy", "1em")
-        .style("text-anchor", "middle")
-        .style('font-size', '12px')
-        .text("IMDB score");
+    vis.svg
+      .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - vis.margin.left)
+      .attr("x", 0 - vis.height / 2)
+      .attr("dy", "1em")
+      .style("text-anchor", "middle")
+      .style("font-size", "12px")
+      .text("IMDB score");
 
     this.wrangleData();
   }
@@ -73,8 +79,8 @@ class PlotVis {
     vis.platformCounts = {
       Netflix: 0,
       Hulu: 0,
-      'Disney+': 0,
-      'Prime Video': 0,
+      "Disney+": 0,
+      "Prime Video": 0,
     };
 
     vis.sortablePlatforms = [];
@@ -83,39 +89,47 @@ class PlotVis {
 
     console.log(selectedGenres);
 
-    filteredData.map(item => {
-      if (item.Netflix === 1 &&
-          (selectedGenres.length === 0 ||
-              selectedGenres.filter((value) =>
-                  item.Genres.split(",").includes(value)
-              ).length !== 0)) {
+    filteredData.map((item) => {
+      if (
+        item.Netflix === 1 &&
+        (selectedGenres.length === 0 ||
+          selectedGenres.filter((value) =>
+            item.Genres.split(",").includes(value)
+          ).length !== 0)
+      ) {
         vis.platformCounts.Netflix += 1;
       }
 
-      if (item.Hulu === 1 &&
-          (selectedGenres.length === 0 ||
-              selectedGenres.filter((value) =>
-                  item.Genres.split(",").includes(value)
-              ).length !== 0)) {
+      if (
+        item.Hulu === 1 &&
+        (selectedGenres.length === 0 ||
+          selectedGenres.filter((value) =>
+            item.Genres.split(",").includes(value)
+          ).length !== 0)
+      ) {
         vis.platformCounts.Hulu += 1;
       }
 
-      if (item['Disney+'] === 1 &&
-          (selectedGenres.length === 0 ||
-              selectedGenres.filter((value) =>
-                  item.Genres.split(",").includes(value)
-              ).length !== 0)) {
-        vis.platformCounts['Disney+'] += 1;
+      if (
+        item["Disney+"] === 1 &&
+        (selectedGenres.length === 0 ||
+          selectedGenres.filter((value) =>
+            item.Genres.split(",").includes(value)
+          ).length !== 0)
+      ) {
+        vis.platformCounts["Disney+"] += 1;
       }
 
-      if (item['Prime Video'] === 1 &&
-          (selectedGenres.length === 0 ||
-              selectedGenres.filter((value) =>
-                  item.Genres.split(",").includes(value)
-              ).length !== 0)) {
-        vis.platformCounts['Prime Video'] += 1;
+      if (
+        item["Prime Video"] === 1 &&
+        (selectedGenres.length === 0 ||
+          selectedGenres.filter((value) =>
+            item.Genres.split(",").includes(value)
+          ).length !== 0)
+      ) {
+        vis.platformCounts["Prime Video"] += 1;
       }
-    })
+    });
 
     console.log(vis.platformCounts);
 
@@ -124,75 +138,110 @@ class PlotVis {
       vis.sortablePlatforms.push([platform, vis.platformCounts[platform]]);
     }
 
-    vis.sortablePlatforms.sort(function(a, b) {
+    vis.sortablePlatforms.sort(function (a, b) {
       return b[1] - a[1];
     });
 
     console.log(vis.sortablePlatforms);
 
-    document.getElementById("top-platform-count").innerHTML = '<b>' + vis.numberWithCommas(vis.sortablePlatforms[0][1]) + '</b>';
-    document.getElementById("top-platform").innerHTML = vis.numberWithCommas(vis.sortablePlatforms[0][0]);
-    document.getElementById("second-platform-count").innerHTML = vis.numberWithCommas(vis.sortablePlatforms[1][1]);
-    document.getElementById("second-platform").innerHTML = vis.numberWithCommas(vis.sortablePlatforms[1][0]);
-    document.getElementById("third-platform-count").innerHTML = vis.numberWithCommas(vis.sortablePlatforms[2][1]);
-    document.getElementById("third-platform").innerHTML = vis.numberWithCommas(vis.sortablePlatforms[2][0]);
-    document.getElementById("fourth-platform-count").innerHTML = vis.numberWithCommas(vis.sortablePlatforms[3][1]);
-    document.getElementById("fourth-platform").innerHTML = vis.numberWithCommas(vis.sortablePlatforms[3][0]);
+    document.getElementById("top-platform-count").innerHTML =
+      "<b>" + vis.numberWithCommas(vis.sortablePlatforms[0][1]) + "</b>";
+    document.getElementById("top-platform").innerHTML = vis.numberWithCommas(
+      vis.sortablePlatforms[0][0]
+    );
+    document.getElementById(
+      "second-platform-count"
+    ).innerHTML = vis.numberWithCommas(vis.sortablePlatforms[1][1]);
+    document.getElementById("second-platform").innerHTML = vis.numberWithCommas(
+      vis.sortablePlatforms[1][0]
+    );
+    document.getElementById(
+      "third-platform-count"
+    ).innerHTML = vis.numberWithCommas(vis.sortablePlatforms[2][1]);
+    document.getElementById("third-platform").innerHTML = vis.numberWithCommas(
+      vis.sortablePlatforms[2][0]
+    );
+    document.getElementById(
+      "fourth-platform-count"
+    ).innerHTML = vis.numberWithCommas(vis.sortablePlatforms[3][1]);
+    document.getElementById("fourth-platform").innerHTML = vis.numberWithCommas(
+      vis.sortablePlatforms[3][0]
+    );
 
-
-
-    vis.finalPlatform = Object.keys(vis.platformCounts).find(key => vis.platformCounts[key] === Math.max(vis.platformCounts.Netflix,
-        vis.platformCounts.Hulu,
-        vis.platformCounts["Disney+"],
-        vis.platformCounts["Prime Video"]));
+    vis.finalPlatform = Object.keys(vis.platformCounts).find(
+      (key) =>
+        vis.platformCounts[key] ===
+        Math.max(
+          vis.platformCounts.Netflix,
+          vis.platformCounts.Hulu,
+          vis.platformCounts["Disney+"],
+          vis.platformCounts["Prime Video"]
+        )
+    );
 
     console.log(vis.finalPlatform);
 
-
-    document.getElementById('final-chosen-platform').innerHTML = 'We think that ' +  vis.finalPlatform + ' is the best platform for you!'
-    document.getElementById('fcp-description').innerHTML =  'Based on your viewing preferences, we think <b>' +  vis.finalPlatform + '</b> is most ' +
-    'suited for you because it has <b>' + vis.numberWithCommas(vis.platformCounts[vis.finalPlatform]) + '</b> movies that: <br /> ' +
-    'have an <b>IMDb</b> rating of at least <b>' + document.getElementById("IMDb").value + '</b>, <br />have a <b>Rotten Tomatoes</b> rating of at least <b>' + document.getElementById("rottenTomatoes").value +
-    '</b>, <br />and fall under your preferred genres: <b>' + $("#genres").val().join(', ') + '</b>'
+    document.getElementById("final-chosen-platform").innerHTML =
+      "We think that " + vis.finalPlatform + " is the best platform for you!";
+    document.getElementById("fcp-description").innerHTML =
+      "Based on your viewing preferences, we think " +
+      vis.finalPlatform +
+      " is most " +
+      "suited for you because it has " +
+      vis.numberWithCommas(vis.platformCounts[vis.finalPlatform]) +
+      " movies that " +
+      "have an <b>IMDb</b> rating of at least " +
+      document.getElementById("IMDb").value +
+      "</b>, have a <b>Rotten Tomatoes</b> rating of at least <b>" +
+      document.getElementById("rottenTomatoes").value +
+      "</b>, and fall under your preferred genres: <b>" +
+      $("#genres").val().join(", ") +
+      "</b>";
 
     let link = vis.assignLink(vis.finalPlatform);
 
-    document.getElementById('fcp-image-container').innerHTML = '' +
-        '<a id="fcp-image" href="' + link + '" target="_blank">' +
-          '<img class="final-image" src="img/' + vis.finalPlatform + '.png" />' +
-        '</a>'
+    document.getElementById("fcp-image-container").innerHTML =
+      "" +
+      '<a id="fcp-image" href="' +
+      link +
+      '" target="_blank">' +
+      '<img class="final-image" src="img/' +
+      vis.finalPlatform +
+      '.png" />' +
+      "</a>";
 
-    if (plotPlatform !== '') {
+    if (plotPlatform !== "") {
       vis.displayData = filteredData.filter((movie) => {
         return (
-            movie[plotPlatform] === 1 &&
-            (selectedGenres.length === 0 ||
-                selectedGenres.filter((value) =>
-                    movie.Genres.split(",").includes(value)
-                ).length !== 0) &&
-            movie["Rotten Tomatoes"] !== null
+          movie[plotPlatform] === 1 &&
+          (selectedGenres.length === 0 ||
+            selectedGenres.filter((value) =>
+              movie.Genres.split(",").includes(value)
+            ).length !== 0) &&
+          movie["Rotten Tomatoes"] !== null
         );
       });
     }
 
-    if (plotPlatform === '') {
-      console.log('plotPlatform all')
-      vis.displayData = filteredData.filter(movie => {
-        return (selectedGenres.length === 0 ||
+    if (plotPlatform === "") {
+      console.log("plotPlatform all");
+      vis.displayData = filteredData.filter((movie) => {
+        return (
+          (selectedGenres.length === 0 ||
             selectedGenres.filter((value) =>
-                movie.Genres.split(",").includes(value)
-            ).length !== 0) && movie['Rotten Tomatoes'] !== null && movie['IMDb'] !== 0;
+              movie.Genres.split(",").includes(value)
+            ).length !== 0) &&
+          movie["Rotten Tomatoes"] !== null &&
+          movie["IMDb"] !== 0
+        );
       });
     }
-
-
 
     vis.updateVis();
   }
 
   updateVis() {
     let vis = this;
-
 
     vis.x.domain(d3.extent(filteredData, (d) => d["Rotten Tomatoes"]));
     // .domain(d3.extent(vis.displayData, d => d['Rotten Tomatoes'])).nice()
@@ -208,56 +257,38 @@ class PlotVis {
     // console.log(vis.shape('1'));
 
     const colorSelector = (platform, d) => {
-
-      if (platform === '') {
-        if (d.Netflix === 1){
-          return '#e50914';
-        }
-
-        else if (d.Hulu === 1){
-          return '#7de39f';
-        }
-
-        else if (d['Prime Video'] === 1){
-          return '#edbc51';
-        }
-
-        else {
-          return '#66a1ed';
+      if (platform === "") {
+        if (d.Netflix === 1) {
+          return "#e50914";
+        } else if (d.Hulu === 1) {
+          return "#7de39f";
+        } else if (d["Prime Video"] === 1) {
+          return "#edbc51";
+        } else {
+          return "#66a1ed";
         }
       }
 
-      if (platform === 'Netflix'){
-        return '#e50914';
+      if (platform === "Netflix") {
+        return "#e50914";
+      } else if (platform === "Hulu") {
+        return "#7de39f";
+      } else if (platform === "Prime Video") {
+        return "#edbc51";
+      } else {
+        return "#66a1ed";
       }
-
-      else if (platform === 'Hulu'){
-        return '#7de39f';
-      }
-
-      else if (platform === 'Prime Video'){
-        return '#edbc51';
-      }
-
-      else {
-        return '#66a1ed';
-      }
-
-    }
+    };
 
     d3.select("#tooltip").attr("hidden", true);
 
-    let circle = vis.svg.selectAll("circle")
-        .data(vis.displayData, function(d) {
-          return d.Title;
-        });
+    let circle = vis.svg
+      .selectAll("circle")
+      .data(vis.displayData, function (d) {
+        return d.Title;
+      });
 
-    circle
-        .exit()
-        .transition()
-        .duration(800)
-        .attr('opacity', 0)
-        .remove();
+    circle.exit().transition().duration(800).attr("opacity", 0).remove();
 
     circle
       .enter()
@@ -283,18 +314,19 @@ class PlotVis {
         // console.log(IMDbScore)
 
         //Update the tooltip position and value
-        let platformList = ["Netflix", "Disney+", "Prime Video", "Hulu"].filter(ptfm => d[ptfm])
+        let platformList = ["Netflix", "Disney+", "Prime Video", "Hulu"].filter(
+          (ptfm) => d[ptfm]
+        );
 
         let html =
-
-            // `<span>${["Netflix", "Disney+", "Prime Video", "Hulu"].filter(ptfm => d[ptfm]).join(', ')}</span>` +
+          // `<span>${["Netflix", "Disney+", "Prime Video", "Hulu"].filter(ptfm => d[ptfm]).join(', ')}</span>` +
           "<span style='font-weight: bold;'>" +
           d.Title +
           "</span><br/>" +
-            "<span>" +
-            `Platform${platformList.length > 1 ? 's: ' : ': '}` +
-            `${platformList.join(', ')}` +
-            "</span><br/>" +
+          "<span>" +
+          `Platform${platformList.length > 1 ? "s: " : ": "}` +
+          `${platformList.join(", ")}` +
+          "</span><br/>" +
           "<span>" +
           "Genres: " +
           d.Genres.replace(/,(?=[^\s])/g, ", ") +
@@ -310,7 +342,7 @@ class PlotVis {
           "</span><br/>" +
           "<span>" +
           "Age rating: " +
-            vis.ageRatingNullHandler(d["Age"])+
+          vis.ageRatingNullHandler(d["Age"]) +
           "</span><br/>";
 
         d3.select("#tooltip")
@@ -325,16 +357,14 @@ class PlotVis {
         //Hide the tooltip
         d3.select("#tooltip").attr("hidden", true);
       })
-      .attr('opacity', '0')
+      .attr("opacity", "0")
       .merge(circle)
       .attr("cx", (d) => vis.x(d["Rotten Tomatoes"]))
       .attr("cy", (d) => vis.y(d["IMDb"]))
       .transition()
       .duration(800)
-      .attr("fill", d => colorSelector(plotPlatform, d))
+      .attr("fill", (d) => colorSelector(plotPlatform, d))
       .attr("opacity", "0.7");
-
-
 
     vis.svg.select(".y-axis").transition().call(vis.yAxis);
     vis.svg.select(".x-axis").transition().call(vis.xAxis);
@@ -342,33 +372,25 @@ class PlotVis {
 
   numberWithCommas = (x) => {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-  }
+  };
 
   assignLink = (platform) => {
-    if (platform === 'Netflix') {
-      return 'https://www.netflix.com/'
+    if (platform === "Netflix") {
+      return "https://www.netflix.com/";
+    } else if (platform === "Prime Video") {
+      return "https://www.primevideo.com/";
+    } else if (platform === "Disney+") {
+      return "https://www.disneyplus.com/";
+    } else {
+      return "https://www.hulu.com/welcome";
     }
-
-    else if (platform === 'Prime Video') {
-      return 'https://www.primevideo.com/'
-    }
-
-    else if (platform === 'Disney+') {
-      return 'https://www.disneyplus.com/'
-    }
-
-    else {
-      return 'https://www.hulu.com/welcome'
-    }
-  }
+  };
 
   ageRatingNullHandler = (rating) => {
     if (rating === null) {
-      return 'not rated'
+      return "not rated";
+    } else {
+      return rating;
     }
-
-    else {
-      return rating
-    }
-  }
+  };
 }
